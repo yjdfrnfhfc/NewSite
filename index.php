e3a6a4602518afba637f212620a871ecaff6e6ef
yjdfrnfhfc <yjdfrnfhfc@gmail.com>
2017-01-04 21:36:16 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <input placeholder="keyword">
+            <input placeholder="marker">
+            <input placeholder="quantity">
+            <input placeholder="quality">
+            <input type="date" placeholder="observed">
+            <input placeholder="link source">
+            <input placeholder="link point">
+            <button>Commit</button>
+        </form>
+        
+    </body>
+</html>
